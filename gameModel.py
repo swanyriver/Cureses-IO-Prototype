4035e3e54f1909415dc5feeb2e643d3b9d264b07
Brandon Swanson <swanyriver@gmail.com>
2016-06-27 23:08:08 -0700

    fixed corner bug by not using last row

--- gameModel.py
+++ gameModel.py
@@ -8,14 +8,17 @@ class Game():
     def __init__(self, height, width):
         self.character = Character(height // 2, width // 2)
         self.maxX = width
-        self.maxY = height
+
+        # this is a hack for now, to solve a problem with curses difficulty writing a character in the very bottom right
+        # http://stackoverflow.com/questions/36387625/curses-calling-addch-on-the-bottom-right-corner
+        self.maxY = height - 1
 
     def getCharPos(self):
         return self.character.y, self.character.x
 
     #this is a request from the controller, not an imitative, game model will determine if move is possible
     def moveCharecter(self, ydelta, xdelta):
-        if 0 <= self.character.x + xdelta <= self.maxX:
+        if 0 <= self.character.x + xdelta < self.maxX:
             self.character.x += xdelta
-        if 0 <= self.character.y + ydelta <= self.maxY:
+        if 0 <= self.character.y + ydelta < self.maxY:
             self.character.y += ydelta
